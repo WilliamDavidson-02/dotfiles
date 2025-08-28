ba480167794a084a0ba8f844f34e0c30f0b29a62
William Davidson <wj.davidson@outlook.com>
2025-08-28 19:32:19 +0200

    chore: hide telescope preview

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,21 +1,28 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', function()
-	builtin.find_files({
-		find_command = {
-			"rg",
-			"--files",
-			"--hidden",
-			"--glob", "!node_modules/**",
-			"--glob", "!.git/**",
-		},
-	})
+  builtin.find_files({
+    previewer = false,  -- hide preview window
+    find_command = {
+      "rg",
+      "--files",
+      "--hidden",
+      "--glob", "!node_modules/**",
+      "--glob", "!.git/**",
+    },
+  })
 end, { desc = 'Telescope find files ignoring node_modules' })
 
-vim.keymap.set("n", "<leader>fg", require('telescope.builtin').live_grep, {})
-vim.keymap.set("n", "<leader>fw", function()
-  require('telescope.builtin').live_grep({
-    default_text = vim.fn.expand("<cword>"),
+vim.keymap.set("n", "<leader>fg", function()
+  builtin.live_grep({
+    previewer = false,  -- hide preview window
   })
-end, { desc = "Live grep word under cursor" })
+end, { desc = "Telescope live grep without preview" })
+
+vim.keymap.set("n", "<leader>fw", function()
+  builtin.live_grep({
+    default_text = vim.fn.expand("<cword>"),
+    previewer = false,  -- hide preview window
+  })
+end, { desc = "Live grep word under cursor without preview" })
 
