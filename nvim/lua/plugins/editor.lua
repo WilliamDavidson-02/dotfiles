be495e4a1fdcb9a4833e183485dba298e2a67dfd
William Davidson <wj.davidson@outlook.com>
2025-09-14 16:09:08 +0200

    feat(nvim): colorized css colors plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -2,6 +2,14 @@ return {
     {
         "MeanderingProgrammer/render-markdown.nvim",
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-        enabled = true, 
+        enabled = true,
     },
+
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup()
+        end,
+    }
+
 }
